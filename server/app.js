131a01697271f4d19d6afec313f862a37ed54db4
vvpvvp <alias8516@sina.com.cn>
2016-08-26 16:19:27 +0800

    fix bug:设定返回结果无法存储大的数值

--- server/app.js
+++ server/app.js
@@ -11,8 +11,12 @@ var app = global.app = express();
 //app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
