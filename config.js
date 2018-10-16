3b5cad58f368da3f31eb6a965d3b497553a7179c
vvpvvp <alias8516@sina.com.cn>
2018-10-16 10:30:04 +0800

    fix: 放开config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  'mysql': {
+    'connectionLimit' : 20,
+    'host': 'localhost',
+    'user':"root",
+    'password':"root",
+    "database":"umock"
+  },
+  'port': 8000
+};
+module.exports = config;
